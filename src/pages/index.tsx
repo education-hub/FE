5118fbe2789bde205261bb7b95bae4e8316ca27a
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-05-11 22:46:28 +0700

    slicing: navbar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home: FC = () => {
   return (
     <div>
-      <div>
-        <h1>Home Page</h1>
-        <Link to="/login">
-          <h1>Go to Login page</h1>
-        </Link>
-        <Link to="/login">
-          <h1>Go to Register page</h1>
-        </Link>
-      </div>
+      <Navbar />
+      <h1>Home Page</h1>
+      <Link to="/login">
+        <h1>Go to Login page</h1>
+      </Link>
+      <Link to="/login">
+        <h1>Go to Register page</h1>
+      </Link>
     </div>
   );
 };
