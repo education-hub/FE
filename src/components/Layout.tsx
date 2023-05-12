30b0f2b85213bbb6614d8c80f0adf8e4c983c792
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-05-12 13:01:29 +0700

    slicing: fix navbar component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react";
-import { Navbar } from "./Navbar";
+import Navbar from "./Navbar";
 import { Footer } from "./Footer";
 
 interface Props {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,28 +44,6 @@ const Navbar: FC = () => {
       </div>
     </nav>
   );
-  // (
-  //   <div className="navbar bg-[#0A142F] px-32 sticky top-0 text-gray-300 z-50">
-  //     <div className="flex-1"></div>
-  //     <div data-theme="mytheme" className="flex-none bg-inherit">
-  //       <ul className="menu menu-horizontal px-1 ">
-  //         <li tabIndex={0} className="dropdown dropdown-end">
-  //           <a className="active:bg-[#080f25] hover:bg-inherit">
-  //             <svg
-  //               className="fill-current"
-  //               xmlns="http://www.w3.org/2000/svg"
-  //               width="30"
-  //               height="30"
-  //               viewBox="0 0 24 24"
-  //             >
-  //               <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-  //             </svg>
-  //           </a>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Navbar;
