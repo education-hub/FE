68eed1a15b002b7e854a46b0013b3283a0f3109d
IrwanFicoFar <irwanhadi9@gmail.com>
2023-05-15 10:25:01 +0700

    resolve unsuble import , change React to FC

--- src/pages/student/Transaction.tsx
+++ src/pages/student/Transaction.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-
+import { FC } from "react";
 import { Layout } from "../../components/Layout";
 import { ButtonSubmit } from "../../components/Button";
 
-const Transaction = () => {
+const Transaction: FC = () => {
   return (
     <Layout>
       <div className="p-20">
