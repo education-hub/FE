73d831dff13d21339ab2380e4834d42b329d9297
IrwanFicoFar <irwanhadi9@gmail.com>
2023-05-20 02:28:59 +0700

    fix: error validation combobox, feat: CRUD extracurricular, CRUD FAQ, CRUD Quiz

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -1,70 +1,60 @@
 import { FC, Fragment, useState } from "react";
 import { Transition, Combobox } from "@headlessui/react";
 import { TbArrowsMoveVertical, TbCheck } from "react-icons/tb";
+import { UseFormSetValue } from "react-hook-form";
+
+import { Schema } from "../pages/admin/AddSchool";
 
 interface ComboBoxType {
-  defaultFill: string;
   title: string;
-  data: any[];
-  selected: {
-    id?: number;
-    id_provinsi?: string;
-    id_kota?: string;
-    id_kecamatan?: string;
-    nama?: string;
-  } | null;
-  setSelected: React.Dispatch<
-    React.SetStateAction<{
-      id?: number;
-      id_provinsi?: string;
-      id_kota?: string;
-      id_kecamatan?: string;
-      nama?: string;
-    } | null>
-  >;
-  name: string;
+  datas: any[];
+  name: string | any;
   register?: any;
-  error: string;
+  error?: string;
+  setValue: UseFormSetValue<Schema>;
+  onChange?: (value: number) => void;
 }
 
 interface Location {
   nama: string;
 }
 
-export const ComboBox: FC<Partial<ComboBoxType>> = (props) => {
-  const {
-    title,
-    data,
-    selected,
-    setSelected,
-    register,
-    name,
-    error,
-    defaultFill,
-  } = props;
-
+export const ComboBox: FC<ComboBoxType> = (props) => {
+  const { title, datas, register, name, error, onChange, setValue } = props;
+  const [selected, setSelected] = useState<any>([]);
   const [query, setQuery] = useState("");
-  const filteredProvinces = data?.filter((data) =>
-    data.nama.toLowerCase().includes(query.toLowerCase())
-  );
+  const filteredDatas =
+    query === ""
+      ? datas
+      : datas.filter((data) =>
+          data.nama
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
     <div className="text-@dark">
-      <p className="block text-gray-700 font-bold">{title}</p>
-      <Combobox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <div className="relative z-10 w-full flex items-center cursor-default bg-white h-16 pl-3 text-left focus:outline-none text-lg text-@dark ">
+      <p className="font-bold text-gray-700 block">{title}</p>
+      <Combobox
+        value={selected}
+        onChange={(value) => {
+          setSelected(value);
+          setValue(name, value.nama, { shouldValidate: true });
+          if (onChange) onChange(value.id);
+        }}
+      >
+        <div className="mt-1 relative">
+          <div className="bg-white cursor-default flex h-16 text-left text-lg text-@dark w-full pl-3 z-10 relative items-center focus:outline-none ">
             <Combobox.Input
-              className="w-full relative border-none py-2 pl-3 px-10 leading-5 text-@dark focus:outline-none focus:ring-0"
-              displayValue={(location: Location) =>
-                location?.nama || defaultFill
-              }
-              onChange={(event) => setQuery(event.target.value)}
               {...(register ? register(name) : {})}
+              className="border-none text-@dark w-full py-2 px-10 pl-3 leading-5 relative focus:outline-none focus:ring-0"
+              onChange={(event) => setQuery(event.target.value)}
+              displayValue={(location: Location) => location?.nama}
             />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <Combobox.Button className="flex pr-2 inset-y-0 right-0 absolute items-center">
               <TbArrowsMoveVertical
-                className="h-5 w-5 text-gray-400"
+                className="h-5 text-gray-400 w-5"
                 aria-hidden="true"
               />
             </Combobox.Button>
@@ -76,22 +66,21 @@ export const ComboBox: FC<Partial<ComboBoxType>> = (props) => {
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options className="absolute z-20 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {filteredProvinces?.length === 0 && query !== "" ? (
-                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+            <Combobox.Options className="bg-white rounded-md shadow-lg ring-black mt-1 text-base w-full max-h-60 py-1 ring-1 ring-opacity-5 z-20 absolute overflow-auto sm:text-sm focus:outline-none">
+              {filteredDatas?.length === 0 && query !== "" ? (
+                <div className="cursor-default py-2 px-4 text-gray-700 relative select-none">
                   Nothing found.
                 </div>
               ) : (
-                filteredProvinces?.map((province) => (
+                filteredDatas.map((data) => (
                   <Combobox.Option
-                    key={province.id}
+                    key={data.id}
                     className={({ active }) =>
                       `relative cursor-default select-none py-2 pl-10 pr-4 ${
                         active ? "bg-@blue text-white" : "text-gray-900"
                       }`
                     }
-                    // {...(register ? register(name) : {})}
-                    value={province}
+                    value={data}
                   >
                     {({ selected, active }) => (
                       <>
@@ -100,7 +89,7 @@ export const ComboBox: FC<Partial<ComboBoxType>> = (props) => {
                             selected ? "font-medium" : "font-normal"
                           }`}
                         >
-                          {province.nama}
+                          {data.nama}
                         </span>
                         {selected ? (
                           <span
