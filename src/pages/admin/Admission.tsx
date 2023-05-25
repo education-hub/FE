e7bfd88137b7ef8b4811ca29abad1487c120ef62
IrwanFicoFar <irwanhadi9@gmail.com>
2023-05-25 12:07:49 +0700

    finish responsive student admission

--- src/pages/admin/Admission.tsx
+++ src/pages/admin/Admission.tsx
@@ -48,7 +48,7 @@ const Admission: FC = () => {
   return (
     <LayoutAdmin>
       <div
-        className="p-20 flex flex-col gap-10 h-screen bg-[url(/bg-2.jpg)] bg-center bg-cover"
+        className="p-7 sm:p-20 flex flex-col gap-10 h-full md:h-screen  bg-[url(/bg-2.jpg)] bg-center bg-cover"
         style={{
           backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.5), rgba(255, 255, 255, 0.5)),url(/bg-2.jpg)`,
         }}
@@ -58,30 +58,34 @@ const Admission: FC = () => {
         ) : (
           <div>
             {!noData ? (
-              <div className="grid grid-cols-6 gap-20">
+              <div className="">
                 {student.map((e) => {
                   return (
-                    <>
-                      <div className="flex justify-end">
+                    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 lg:space-x-10 text-lg font-semibold hover:drop-shadow-lg hover:-translate-y-2 mb-10 md:space-x-10 px-10 pt-5 pb-10 bg-[url('/bg-1.jpg')] bg-cover hover:bg-center duration-500">
+                      <div className=" justify-around hidden md:block ">
                         <img
                           src={`https://storage.googleapis.com/prj1ropel/${e.user_image}`}
                           alt=""
-                          className="h-16 w-auto mt-6"
+                          className="h-16 w-16 mt-6 hidden md:block"
                         />
                       </div>
-                      <div className="">
+                      {/* <div className="hidden sm:blok md:hidden"></div> */}
+                      <div className="sol-span-1 sm:col-span-2 lg:col-span-1">
                         <p>Student_id</p>
                         <div className="bg-@light-blue px-3 flex items-center justify-center h-16 text-md sm:text-lg md:text-xl border-2 text-@dark font-medium  focus:outline-none  ">
                           <p>{e.user_id}</p>
                         </div>
                       </div>
-                      <div className="col-span-2">
+                      <div className="hidden md:block lg:hidden"></div>
+                      <div className="sm:col-span-2">
                         <p>Fullname</p>
-                        <div className="bg-@light-blue px-3 flex items-center h-16 text-md sm:text-lg md:text-xl border-2 text-@dark font-medium  focus:outline-none  ">
+                        <div className="bg-@light-blue px-3 flex items-center h-16 w-full text-md sm:text-lg md:text-xl border-2 text-@dark font-medium  focus:outline-none  ">
                           <p>{e.user_name}</p>
                         </div>
                       </div>
-                      <div className="pt-6">
+                      <div className="hidden md:block lg:block xl:hidden"></div>
+                      <div className="hidden md:hidden lg:block xl:hidden"></div>
+                      <div className="pt-6 flex flex-col sm:flex-row ">
                         <ButtonSubmit
                           label="Detail Admission"
                           onClick={() =>
@@ -89,7 +93,7 @@ const Admission: FC = () => {
                           }
                         />
                       </div>
-                      <div className="pt-6">
+                      <div className="pt-6 flex justify-end flex-col sm:flex-row">
                         <ButtonSubmit
                           label="Update Progress"
                           onClick={() =>
@@ -97,7 +101,7 @@ const Admission: FC = () => {
                           }
                         />
                       </div>
-                    </>
+                    </div>
                   );
                 })}
               </div>
