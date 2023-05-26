3a365dc6f40b8e96037cc6549bbf47f7924176f7
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-05-26 10:53:22 +0700

    student-add-condition-if-data-null-in-register-transaction-page

--- src/pages/student/Transaction.tsx
+++ src/pages/student/Transaction.tsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
-import Swal from "sweetalert2";
 
 interface TransactionType {
   school_name: string;
@@ -15,6 +14,7 @@ interface TransactionType {
 
 const Transaction: FC = () => {
   const [datas, setDatas] = useState<TransactionType[]>([]);
+  const [noData, setNoData] = useState<boolean>(true);
   const [cookie] = useCookies(["tkn"]);
 
   useEffect(() => {
@@ -32,51 +32,61 @@ const Transaction: FC = () => {
       .then((res) => {
         const { data } = res.data;
         setDatas(data);
+        setNoData(false);
       })
-      .catch((error) => {
-        const { message } = error.response.data;
-        Swal.fire({
-          icon: "info",
-          title: "Failed",
-          text: message,
-          showCancelButton: false,
-        });
+      .catch(() => {
+        setNoData(true);
       });
   }
 
   return (
     <Layout>
-      <div className="p-20">
-        {datas.map((data) => {
-          return (
-            <div
-              className="grid grid-cols-8 gap-4 tracking-normal mt-5"
-              key={data.school_id}
-            >
-              <div className="flex items-center justify-center">
-                <img
-                  src={`https://storage.googleapis.com/prj1ropel/${data.school_image}`}
-                  alt="school_pict"
-                  className="max-w-[150px] max-h-[100px]"
-                />
-              </div>
-              <div className="col-span-4 grid grid-rows-3">
-                <p className="font-semibold text-gray-500">School Name</p>
-                <div className="row-span-2 flex items-center bg-@light-blue w-full px-3 h-full">
-                  {data.school_name}
-                </div>
-              </div>
-              <div className="col-span-3 grid grid-rows-3 h-full w-full">
-                <p className="text-white">Button Detail Transaction</p>
-                <div className="row-span-2">
-                  <Link to={`/student/transactions/${data.school_id}`}>
-                    <ButtonSubmit label="Detail Transaction" />
-                  </Link>
-                </div>
-              </div>
+      <div
+        className="p-7 sm:p-20 flex flex-col gap-10 h-full md:h-screen bg-[url(/bg-5.jpg)] bg-top bg-cover"
+        style={{
+          backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.5), rgba(255, 255, 255, 0.5)),url(/bg-5.jpg)`,
+        }}
+      >
+        <div>
+          {!noData ? (
+            <div>
+              {datas.map((data) => {
+                return (
+                  <div
+                    className="grid grid-cols-8 gap-4 tracking-normal mt-5"
+                    key={data.school_id}
+                  >
+                    <div className="flex items-center justify-center">
+                      <img
+                        src={`https://storage.googleapis.com/prj1ropel/${data.school_image}`}
+                        alt="school_pict"
+                        className="max-w-[150px] max-h-[100px]"
+                      />
+                    </div>
+                    <div className="col-span-4 grid grid-rows-3">
+                      <p className="font-semibold text-gray-500">School Name</p>
+                      <div className="row-span-2 flex items-center bg-@light-blue w-full px-3 h-full">
+                        {data.school_name}
+                      </div>
+                    </div>
+                    <div className="col-span-3 grid grid-rows-3 h-full w-full">
+                      <p className="text-white">Button Detail Transaction</p>
+                      <div className="row-span-2">
+                        <Link to={`/student/transactions/${data.school_id}`}>
+                          <ButtonSubmit label="Detail Transaction" />
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
+          ) : (
+            <div className="text-7xl font-medium flex items-center justify-center py-18 h-full text-gray-500">
+              <p>Data Not Found</p>
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   );
