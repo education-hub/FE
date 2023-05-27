b3bb4cab4e046d77050e4a7a40ab5a27c41f87f1
IrwanFicoFar <irwanhadi9@gmail.com>
2023-05-27 22:23:35 +0700

    reparation not found

--- src/pages/admin/TestResult.tsx
+++ src/pages/admin/TestResult.tsx
@@ -78,51 +78,11 @@ const TestResult: FC = () => {
                 ))}
               </div>
             ) : (
-              <div className="text-7xl font-medium flex items-center justify-center h-full text-gray-300">
+              <div className="text-7xl font-medium flex py-24 justify-center h-screen text-gray-500">
                 <p>Data Not Found</p>
               </div>
             )}
           </div>
-          {/* <div className="pt-20 z-50 pl-20 pb-20">
-            {!noData ? (
-              <div>
-                {TestResult.map((e, index) => (
-                  <div className=" flex gap-10 mb-10">
-                    <div className="pt-6">
-                      <div className="bg-@light-blue px-3 flex justify-center items-center h-16 w-24 text-md sm:text-lg md:text-xl border-2 text-@dark font-medium  focus:outline-none  ">
-                        <p className="text-center">{index + 1}</p>
-                      </div>
-                    </div>
-                    <div>
-                      <p>Email</p>
-                      <div className="bg-@light-blue px-3 flex items-center h-16 w-96 text-md sm:text-lg md:text-xl border-2 text-@dark font-medium  focus:outline-none  ">
-                        <p>{e.email}</p>
-                      </div>
-                    </div>
-                    <div className="pt-6">
-                      <div
-                        className={`${
-                          e.result === "Pass" ? "bg-@blue" : "bg-@orange"
-                        } duration-500 px-7 h-16 w-32 flex justify-center items-center text-white font-semibold`}
-                      >
-                        <p>{e.result.toUpperCase()}</p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <div className="text-7xl font-medium flex items-center justify-center h-full text-gray-300">
-                <p>Data Not Found</p>
-              </div>
-            )}
-          </div> */}
-          {/* <div
-            className="relative hidden md:block bg-cover bg-center h-screen"
-            // style={{
-            //   backgroundImage: `url(/result2.jpg)`,
-            // }}
-          ></div> */}
         </div>
       )}
     </LayoutAdmin>
